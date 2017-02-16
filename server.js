7e55d96717a1e1e0576097cff2d5b69d612de7c9
Shweta Bhartia <shweta.bhartia93@gmail.com>
2017-02-16 11:23:13 -0500

    Addes POST

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var app = express();
-PORT=process.env.PORT || 3000;
+PORT = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
 
 var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
+app.use(bodyParser.json());
 
-app.get('/about', function (req, res) {
+app.get('/about', function(req, res) {
 	res.send('Abous Us!');
+});
+
+app.post('/users', function (req, res) {
+	var body = req.body;
 });
 
 app.use(express.static(__dirname + '/public'));
